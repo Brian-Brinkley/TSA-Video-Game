7eab6d1c156cde06078ab86f2ea337edb4925323
baby-wood <bwood@pcnet-wtc.org>
2026-02-26 09:15:24 -0600

    Added House level

---
+++ Level/WallTilesComplete.tsx
@@ -0,0 +1,85 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="WallTilesComplete" tilewidth="32" tileheight="32" tilecount="24" columns="8">
+ <image source="Documents/WallTilesComplete.png" width="256" height="96"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="3">
+   <object id="3" x="0.00878964" y="0" width="31.9912" height="18.0382"/>
+   <object id="4" x="-0.0106539" y="18.0743" width="6.94726" height="13.8732"/>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <objectgroup draworder="index" id="4">
+   <object id="3" x="0.0401515" y="0.00887784" width="31.9421" height="17.9774"/>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.0132408" y="0.00868639" width="31.9828" height="17.9898"/>
+   <object id="4" x="24.9961" y="18.0212" width="7.00391" height="13.9844"/>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.0078125" y="0" width="31.9922" height="17.9979"/>
+  </objectgroup>
+ </tile>
+ <tile id="4">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.0124201" y="0.00111231" width="6.98214" height="17.9871"/>
+  </objectgroup>
+ </tile>
+ <tile id="5">
+  <objectgroup draworder="index" id="3">
+   <object id="4" x="0.0353535" y="0.00284091" width="31.9597" height="17.9972"/>
+   <object id="5" x="0.00390625" y="18.0625" width="6.99219" height="13.9648"/>
+  </objectgroup>
+ </tile>
+ <tile id="8">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.00390625" y="-0.125" width="6.9822" height="32.1016"/>
+  </objectgroup>
+ </tile>
+ <tile id="10">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="25.0156" y="0" width="6.98438" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="11">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.0169158" y="13" width="6.95168" height="18.9888"/>
+  </objectgroup>
+ </tile>
+ <tile id="12">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="25.0341" y="0.0213068" width="6.9548" height="17.9393"/>
+  </objectgroup>
+ </tile>
+ <tile id="13">
+  <objectgroup draworder="index" id="3">
+   <object id="2" x="0.0337674" y="0.0117188" width="31.9545" height="22.0234"/>
+   <object id="3" x="25.0024" y="22.0381" width="6.99757" height="9.96189"/>
+  </objectgroup>
+ </tile>
+ <tile id="16">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.0195313" y="13.0056" width="31.9727" height="18.9583"/>
+   <object id="3" x="0.0234375" y="0.125" width="6.97656" height="12.8444"/>
+  </objectgroup>
+ </tile>
+ <tile id="17">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.0262626" y="13" width="31.9737" height="18.9794"/>
+  </objectgroup>
+ </tile>
+ <tile id="18">
+  <objectgroup draworder="index" id="6">
+   <object id="6" x="25.0591" y="0.0434783" width="6.9375" height="31.9287"/>
+   <object id="7" x="0.015625" y="12.9744" width="25.0241" height="18.9972"/>
+  </objectgroup>
+ </tile>
+ <tile id="19">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="25.0332" y="12.991" width="6.95168" height="18.9888"/>
+  </objectgroup>
+ </tile>
+</tileset>
